ef6a3fdbfc9be66c85b2ca2cb4b3ebe53e43f197
bparshall <bparshall@ezmetrology.com>
2016-10-10 13:48:54 -0400

    Trying this now.

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ var commands = [
 					[/ogrebomb/i,processOgreBomb],
 					[/crashPLZ/,processForceCrash],
 					[/\/sob/i,processSob],
-					[/A+N+T+I/,processAchuuu],
-			   ];
+					[/A+N+T+I+\b/,processAchuuu],
+			   ]; 
 const ytdl = require('ytdl-core');
 const streamOptions = { seek: 0, volume: 1 };
 
